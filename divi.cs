32847bc1fac089bb9352758c06344b94976b8cd6
ventas <154559613+AlberVentur@users.noreply.github.com>
2024-07-24 23:50:17 -0400

    Add files via upload

---
+++ divi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            
+            Console.Write("Ingrese el primer número: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+
+            
+            Console.Write("Ingrese el segundo número: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            
+            double resultado = numero1 / numero2;
+
+            
+            Console.WriteLine("El resultado de la división es: " + resultado);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error: Ingrese un número válido.");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error: No se puede dividir por cero.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ocurrió un error: " + ex.Message);
+        }
+    }
+}
